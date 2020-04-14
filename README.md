1160f5a2ecef25effc9847794b9ffade94f16e1a
tom <tomcutler39@gmail.com>
2020-04-14 17:55:21 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,23 @@
+```
+
+ ___  __    ________  ________   ________  ________  ________  ________  ________     
+|\  \|\  \ |\   __  \|\   ___  \|\   ____\|\   __  \|\   __  \|\   __  \|\   __  \    
+\ \  \/  /|\ \  \|\  \ \  \\ \  \ \  \___|\ \  \|\  \ \  \|\  \ \  \|\  \ \  \|\  \   
+ \ \   ___  \ \   __  \ \  \\ \  \ \  \  __\ \   __  \ \   _  _\ \  \\\  \ \  \\\  \  
+  \ \  \\ \  \ \  \ \  \ \  \\ \  \ \  \|\  \ \  \ \  \ \  \\  \\ \  \\\  \ \  \\\  \ 
+   \ \__\\ \__\ \__\ \__\ \__\\ \__\ \_______\ \__\ \__\ \__\\ _\\ \_______\ \_______\
+    \|__| \|__|\|__|\|__|\|__| \|__|\|_______|\|__|\|__|\|__|\|__|\|_______|\|_______|
+                                                                                      
+                                                                                      
+```
+
+# Kangaroo
+
+Super lightweight recon script that just uses sublist3r to search for subdomains, then sorts only looking for 200 OKs following 301 redirects with a 5 second max timeout for any operation. Makes a new directory with the domain's name in the current working directory.
+
+# Usage
+`./recon.sh domain.com`
+
+# To do
+* add more search tools to grab more subdomains passively
+* CNAME records?
---
+++ recon.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+
+red=`tput setaf 1`
+green=`tput setaf 2`
+yellow=`tput setaf 3`
+reset=`tput sgr0`
+
+SECONDS=0
+
+logo(){
+    echo "${yellow}"
+    cat << "EOF"
+ ___  __    ________  ________   ________  ________  ________  ________  ________
+|\  \|\  \ |\   __  \|\   ___  \|\   ____\|\   __  \|\   __  \|\   __  \|\   __  \
+\ \  \/  /|\ \  \|\  \ \  \\ \  \ \  \___|\ \  \|\  \ \  \|\  \ \  \|\  \ \  \|\  \
+ \ \   ___  \ \   __  \ \  \\ \  \ \  \  __\ \   __  \ \   _  _\ \  \\\  \ \  \\\  \
+  \ \  \\ \  \ \  \ \  \ \  \\ \  \ \  \|\  \ \  \ \  \ \  \\  \\ \  \\\  \ \  \\\  \
+   \ \__\\ \__\ \__\ \__\ \__\\ \__\ \_______\ \__\ \__\ \__\\ _\\ \_______\ \_______\
+    \|__| \|__|\|__|\|__|\|__| \|__|\|_______|\|__|\|__|\|__|\|__|\|_______|\|_______|
+
+EOF
+echo "${reset}"
+}
+
+recon(){
+    echo -ne "Listing subdomains using Sublist3r...\n"
+    python3 ~/tools/Sublist3r/sublist3r.py -d $1 -v -o ./$1/subdomains.txt > /dev/null
+    echo -e "\rListing subdomains using Sublist3r... Done!"
+
+}
+
+check-ok(){
+    echo -ne "Checking status of listed subdomains...\n"
+    i=1
+    n=$(wc -l < ./$1/subdomains.txt)
+
+    while read LINE; do
+        curl -L --max-redirs 10 -o /dev/null -m 5 --silent --get --write-out "%{http_code} $LINE\n" "$LINE" >> list.txt
+        printf "Completed: $i/$n\r"
+        ((i=i+1))
+    done < ./$1/subdomains.txt
+
+    sed '/^200/ !d' < list.txt > ./$1/domain-status.txt
+    rm list.txt
+    echo "Checking status of listed subdomains... Done!"
+}
+
+main(){
+    clear
+    logo
+    echo "@whichtom"
+    if [[ -d "./$1" ]]; then
+        echo "Target already known"
+    else
+        mkdir ./$1
+    fi
+    
+    recon $1
+    check-ok $1
+    duration=$SECONDS
+    echo "Subdomain reconnaissance completed in: $(($duration / 60)) minutes and $(($duration % 60)) seconds."
+    stty sane
+    tput sgr0
+}
+
+main $1
