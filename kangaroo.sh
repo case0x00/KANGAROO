ac014457045d57a7894ed7938a63b5797b71ef18
tom <tomcutler39@gmail.com>
2020-05-15 15:46:24 +0100

    set httprobe timeout to 3s, moved logo, fixed check-ok output, added aquatone flag, individual check-time calls

--- kangaroo.sh
+++ kangaroo.sh
@@ -11,28 +11,11 @@ chromePath=/usr/bin/google-chrome
 
 SECONDS=0
 
+doaqua=
 domain=
-logo(){
-    echo "${yellow}"
-    cat << "EOF"
- ___  __    ________  ________   ________  ________  ________  ________  ________
-|\  \|\  \ |\   __  \|\   ___  \|\   ____\|\   __  \|\   __  \|\   __  \|\   __  \
-\ \  \/  /|\ \  \|\  \ \  \\ \  \ \  \___|\ \  \|\  \ \  \|\  \ \  \|\  \ \  \|\  \
- \ \   ___  \ \   __  \ \  \\ \  \ \  \  __\ \   __  \ \   _  _\ \  \\\  \ \  \\\  \
-  \ \  \\ \  \ \  \ \  \ \  \\ \  \ \  \|\  \ \  \ \  \ \  \\  \\ \  \\\  \ \  \\\  \
-   \ \__\\ \__\ \__\ \__\ \__\\ \__\ \_______\ \__\ \__\ \__\\ _\\ \_______\ \_______\
-    \|__| \|__|\|__|\|__|\|__| \|__|\|_______|\|__|\|__|\|__|\|__|\|_______|\|_______|
-EOF
-echo "${reset}"
-    echo "${red}"
-    cat << "EOF"
-                                    @whichtom
-EOF
-echo "${reset}"
-}
 
-while getopts ":d:e:" o; do
-    case "${o}" in
+while getopts ":d:e:a" flag; do
+    case "${flag}" in
         d)
             domain=${OPTARG}
             ;;
@@ -45,10 +28,27 @@ while getopts ":d:e:" o; do
         s)
             scope=${OPTARG} # ikinda need to append '/' to the end so it doesnt fuck up comms if its null
             ;;
+        a)
+            doaqua="true"
+            ;;
     esac
 done
 shift $((OPTIND - 1))
 
+logo(){
+    echo "${yellow}"
+    cat << "EOF"
+ ___  __    ________  ________   ________  ________  ________  ________  ________
+|\  \|\  \ |\   __  \|\   ___  \|\   ____\|\   __  \|\   __  \|\   __  \|\   __  \
+\ \  \/  /|\ \  \|\  \ \  \\ \  \ \  \___|\ \  \|\  \ \  \|\  \ \  \|\  \ \  \|\  \
+ \ \   ___  \ \   __  \ \  \\ \  \ \  \  __\ \   __  \ \   _  _\ \  \\\  \ \  \\\  \
+  \ \  \\ \  \ \  \ \  \ \  \\ \  \ \  \|\  \ \  \ \  \ \  \\  \\ \  \\\  \ \  \\\  \
+   \ \__\\ \__\ \__\ \__\ \__\\ \__\ \_______\ \__\ \__\ \__\\ _\\ \_______\ \_______\
+    \|__| \|__|\|__|\|__|\|__| \|__|\|_______|\|__|\|__|\|__|\|__|\|_______|\|_______|
+
+EOF
+echo "${reset}"
+}
 
 set-scope(){
     # check if scope is unset or is an empty string
@@ -61,7 +61,7 @@ set-scope(){
 }
 
 exclusion(){
-    # check if the array excluded is empty
+    # excludes subdomains from check-ok
     if [ ${#excluded[@]} -eq 0 ]; then
         echo -ne "\n${red}::${reset} No subdomain exclusions to apply\n"
     else
@@ -85,28 +85,28 @@ recon(){
 }
 
 check-ok(){
+    # uses httprobe to check whether the subdomain is alive
     i=1
     n=$(wc -l < ./$domain/subdomains.txt)
     
     echo -ne "\r${red}::${reset} Checking status of listed subdomains:\n"
     while read LINE; do
-        echo "$LINE" | httprobe -t 5000 >> ./$domain/responsive.txt
-        progressbar ${i} ${n}
+        echo "$LINE" | httprobe -t 3000 >> ./$domain/responsive.txt
+        progress-bar ${i} ${n}
         ((i=i+1))
     done < ./$domain/subdomains.txt
 
     printf "\n${red}::${reset} Done!\n"
     m=$(wc -l < ./$domain/responsive.txt)
-    echo -ne "${red}::${reset} Total $n subdomains after check. Reduced by $((n - m))\n"
+    echo -ne "${red}::${reset} Total $m subdomains after check. Reduced by $((n - m))\n"
 }
 
 aqua(){
     # aquatone for nice visualization
-    echo -ne "${red}::${reset} Starting aquatone...\n"
     cat ./$domain/responsive.txt | aquatone -chrome-path $chromePath -out ./$domain/aqua-out -threads 5 -silent
 }
 
-progressbar(){
+progress-bar(){
     # nice progress bar
     let progress="(${1}*100/${2}*100)/100"
     let done="(${progress}*6)/10"
@@ -117,9 +117,16 @@ progressbar(){
 
 }
 
+check-time(){
+    taskname=$1
+    duration=$SECONDS
+    echo "${red}::${reset} $taskname completed in: $(($duration / 60)) minutes and $(($duration % 60)) seconds."
+}
+
 main(){
-    clear
+#    clear
     logo
+    echo "${red}@whichtom${reset}"
 
     echo "${red}::${reset} Hitting target $domain..."
     if [[ -d "./$domain" ]]; then
@@ -128,11 +135,21 @@ main(){
         mkdir ./$domain
     fi
 
-    recon $domain
-    check-ok $domain
-    aqua $domain
-    duration=$SECONDS
-    echo "${red}::${reset} Subdomain reconnaissance completed in: $(($duration / 60)) minutes and $(($duration % 60)) seconds."
+#    recon $domain
+#	check-time "Sublist3r"
+
+#    check-ok $domain
+#	check-time "Httprobe"
+
+    if [[ $doaqua == "true" ]]; then
+        echo -ne "${red}::${reset} Starting aquatone...\n"
+        aqua $domain
+        check-time "Aquatone"
+    else
+        echo -ne "${red}::${reset} Aquatone not being used\n"
+    fi
+
+    check-time "Total subdomain reconnaissance"
     stty sane
     tput sgr0
 }
